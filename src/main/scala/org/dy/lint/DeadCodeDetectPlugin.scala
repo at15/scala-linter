a381431f55854bced2ab9e18f13c3a657312bc22
at15 <at15@outlook.com>
2015-05-28 00:11:47 +0800

    Enable two components
    1. change a string literal to 'ICanHizstr..'
    2. division by zero
    
    but not able to find division by one ....

--- src/main/scala/org/dy/lint/DeadCodeDetectPlugin.scala
+++ src/main/scala/org/dy/lint/DeadCodeDetectPlugin.scala
@@ -8,100 +8,31 @@ import nsc.Global
 import nsc.Phase
 import nsc.plugins.Plugin
 import nsc.plugins.PluginComponent
-import nsc.transform.{ Transform, TypingTransformers }
+import nsc.transform.{Transform, TypingTransformers}
 import nsc.symtab.Flags
 
-//
-//class DeadCodeDetectPlugin(val global: Global) extends Plugin {
-//
-//  import global._
-//
-//  val name = "DeadCodeDetect"
-//  val description = "Dead Code Detection for scala"
-////  val components = List[PluginComponent](DivisionByZeroComponent,ReplaceStringComponent)
-//  val components = List[PluginComponent](ReplaceStringComponent)
-//
-//
-//  private object ReplaceStringComponent extends PluginComponent with Transform {
-//
-//    import global._
-//    import global.definitions._
-//
-//    val global = DeadCodeDetectPlugin.this.global
-//    val phaseName = "replace string"
-//    override val runsAfter = List("parser")
-//
-//    def newTransformer(unit: CompilationUnit) = new DeadCodeDetectTransformer(unit)
-//
-//    class DeadCodeDetectTransformer(unit: CompilationUnit) extends Transformer {
-//
-//      // TODO: fill in your logic here
-//      override def transform(tree: Tree): Tree = tree match {
-//        case Literal(Constant(str: String)) => Literal(Constant("ICanHazYourStrngLiterls"))
-//
-//        // don't forget this case, so that tree is actually traversed
-//        case _ => super.transform(tree)
-//      }
-//    }
-//  }
-//
-//
-//
-//  private object DivisionByZeroComponent extends PluginComponent {
-//
-//    import global._
-//
-//    val global = DeadCodeDetectPlugin.this.global
-//    val phaseName = "division by zero"
-//    override val runsAfter = List("refchecks")
-//
-//    def newPhase(prev: Phase) = new DivisionByZeroPhase(prev)
-//
-//    class DivisionByZeroPhase(prev: Phase) extends StdPhase(prev) {
-//      // TODO:what is unit?
-//      override def apply(unit: CompilationUnit) {
-//        for (tree@Apply(Select(rcvr, nme.DIV), List(Literal(Constant(0)))) <- unit.body;
-//             if rcvr.tpe <:< definitions.IntClass.tpe) {
-//          global.reporter.warning(tree.pos, "definitely division by zero")
-//        }
-//
-//                  for (tree@Apply(Select(rcvr, nme.DIV), List(Literal(Constant(1)))) <- unit.body;
-//                       if rcvr.tpe <:< definitions.IntClass.tpe) {
-//                    global.reporter.warning(tree.pos, "definitely division by one")
-//                  }
-////                  super.traverse(tree)
-//      }
-//    }
-//
-//  }
-//
-//}
 
 class DeadCodeDetectPlugin(val global: Global) extends Plugin {
-  import global._ // TODO: import what compiling code has imported?
+
+  import global._
 
   val name = "DeadCodeDetect"
   val description = "Dead Code Detection for scala"
-  val components = List[PluginComponent](ReplaceStringComponent)
+  val components = List[PluginComponent](DivisionByZeroComponent, ReplaceStringComponent)
 
-  // a sample component which is a transformer
-  // which replaces all literal string constants
-  // in the compiled sources
+
   private object ReplaceStringComponent extends PluginComponent with Transform {
 
     import global._
-    import global.definitions._
 
     val global = DeadCodeDetectPlugin.this.global
+    val phaseName = DeadCodeDetectPlugin.this.name
     override val runsAfter = List("parser")
-    // TODO:It must be the same as Plugin name ... I don't know why though.
-    val phaseName = "DeadCodeDetect"
 
     def newTransformer(unit: CompilationUnit) = new DeadCodeDetectTransformer(unit)
 
     class DeadCodeDetectTransformer(unit: CompilationUnit) extends Transformer {
 
-      // TODO: fill in your logic here
       override def transform(tree: Tree): Tree = tree match {
         case Literal(Constant(str: String)) => Literal(Constant("ICanHazYourStrngLiterls"))
 
@@ -110,7 +41,33 @@ class DeadCodeDetectPlugin(val global: Global) extends Plugin {
       }
     }
   }
+
+
+  private object DivisionByZeroComponent extends PluginComponent {
+
+    import global._
+
+    val global = DeadCodeDetectPlugin.this.global
+    val phaseName = "division by zero"
+    override val runsAfter = List("refchecks")
+
+    def newPhase(prev: Phase) = new DivisionByZeroPhase(prev)
+
+    class DivisionByZeroPhase(prev: Phase) extends StdPhase(prev) {
+      // TODO:what is unit?
+      override def apply(unit: CompilationUnit) {
+        for (tree@Apply(Select(rcvr, nme.DIV), List(Literal(Constant(0)))) <- unit.body;
+             if rcvr.tpe <:< definitions.IntClass.tpe) {
+          global.reporter.warning(tree.pos, "definitely division by zero")
+        }
+
+//        for (tree@Apply(Select(rcvr, nme.DIV), List(Literal(Constant(1)))) <- unit.body;
+//             if rcvr.tpe <:< definitions.IntClass.tpe) {
+//          global.reporter.warning(tree.pos, "definitely division by one")
+//        }
+      }
+    }
+
+  }
+
 }
-
-
-
